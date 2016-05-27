5edb77ceef9efe78f51e7457c55bfdadc4aa0161
goldfish819 <goldfish819@hanmail.net>
2016-05-27 18:04:13 +0900

    add volley

--- app/src/main/java/com/test/yuhyegyeong/myapplication/activity/Act1Activity.java
+++ app/src/main/java/com/test/yuhyegyeong/myapplication/activity/Act1Activity.java
@@ -1,6 +1,7 @@
 package com.test.yuhyegyeong.myapplication.activity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -13,11 +14,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.test.yuhyegyeong.myapplication.R;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Map;
 
 /**
  * Created by yuhyegyeong on 2016. 5. 25..
@@ -25,12 +36,11 @@ import java.io.IOException;
 public class Act1Activity extends AppCompatActivity{
 
     private static final int PICK_FILE_REQUEST =1;
-
     EditText act1Title;
     EditText act1Content;
     Button act1BtnCamera;
     ImageView act1Photo;
-
+    private String UPLOAD_URL = "http://text.com";
     private Bitmap bitmap;
     private String KEY_IMAGE = "image";
     private String KEY_NAME = "name";
@@ -95,4 +105,55 @@ public class Act1Activity extends AppCompatActivity{
         String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
         return encodedImage;
     }
+
+    // upload 1
+    private void uploadImage() {
+        //Showing the progress dialog
+        final ProgressDialog loading = ProgressDialog.show(this, "Uploading", "Wait", false, false);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, UPLOAD_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //Dismissing the progress dialog
+                loading.dismiss();
+                //Showing toast message of the response
+                Toast.makeText(Act1Activity.this, response, Toast.LENGTH_SHORT).show();
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //Dismissing the progress dialog
+                        loading.dismiss();
+                        //Showing toast
+                        Toast.makeText(Act1Activity.this, error.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //Converting Bitmap to String
+                String image = getStringImage(bitmap);
+
+                //Getting Image Name
+                String name = act1Title.getText().toString().trim();
+
+                //Creating parameters
+                Map<String, String> params = new Hashtable<String, String>();
+
+                //Adding parameters
+                params.put(KEY_IMAGE, image);
+                params.put(KEY_NAME, name);
+
+                //returning parameters
+                return params;
+            }
+        };
+        //Creating a Request Queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        //Adding request to the queue
+        requestQueue.add(stringRequest);
+    }
+
+    // upload 2
+
 }
